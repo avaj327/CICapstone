e85aaeb631e1116c8f4c4ab76318486936baabd3
avaj327 <johnson.ava327@gmail.com>
2023-02-15 09:00:58 -0500

    moved API auth key to apikey.js, added apikey.js to git ignore

--- USAJobs/test.js
+++ USAJobs/test.js
@@ -1,9 +1,10 @@
 var request = require('request');    
 const FileSystem = require('fs');
+var key = require('./apikey');
             
 var host = 'data.usajobs.gov';  
 var userAgent = 'johnso154@students.rowan.edu';  
-var authKey = 'EEF47C5F-7E2E-4201-A2C9-F6787A7278E1';    
+var authKey = key.authKey;  //note: api not uploaded to github
             
 request({      
     url: 'https://data.usajobs.gov/api/search?JobCategoryCode=2210&Keyword=Software Development&LocationName=Philadelphia, PA',      
